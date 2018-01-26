5a918d8a5777c6d1085057d3787d76615b0fccf9
Chad Darby <cdarby@redhat.com>
2018-01-26 15:42:07 -0500

    spring - adding starter files for labs 2 and 3

---
+++ spring/lab2/src/main/java/com/redhat/coolstore/productcatalog/ProductCatalogService.java
@@ -0,0 +1,21 @@
+package com.redhat.coolstore.productcatalog;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/products")
+public class ProductCatalogService {
+
+    @Value("${coolstore.message:Hello World!}")
+    private String message;
+	
+    @ResponseBody
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)    
+    public String sayHello() {    		
+        return message;
+    }
+	
+}
---
+++ spring/lab3/src/main/java/com/redhat/coolstore/productcatalog/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.redhat.coolstore.productcatalog;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+	public Product findByName(String name);
+	
+}
