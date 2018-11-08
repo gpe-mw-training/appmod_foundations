cfdf6c59ef8c3d475fc01de6c44db8ea643ef9fd
Chad Darby <cdarby@redhat.com>
2018-11-08 16:28:27 -0500

    vert.x - lab 1 solution

--- vert.x/lab1/src/main/java/io/vertx/sample/MyFirstVerticle.java
+++ vert.x/lab1/src/main/java/io/vertx/sample/MyFirstVerticle.java
@@ -7,6 +7,16 @@ public class MyFirstVerticle extends AbstractVerticle {
     @Override
     public void start() {
 
+      // Handle each incoming HTTP request
+
+    vertx.createHttpServer()
+    .requestHandler(req -> {
+            req.response().end(
+              "Hello John: Time on the server is "
+              + java.time.LocalDateTime.now());
+    })
+    .listen(8080);
+
     }
 
 }
